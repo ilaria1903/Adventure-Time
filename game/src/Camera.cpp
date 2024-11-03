a57b67ee21e91469c49964d24f49f289d8dae1a5
ilaria1903 <alexiailaria19@yahoo.com>
2024-11-03 14:23:12 +0200

    test

--- game/src/Camera.cpp
+++ game/src/Camera.cpp
@@ -1,14 +1,14 @@
 #include "../include/Camera.h"
 
 Camera::Camera(int x, int y)
-    : x(x), y(y) {}
+    : x(x), y(y) {}  // This is fine, as initializer lists use the parameters directly
 
 Camera::Camera(const Camera& other)
     : x(other.x), y(other.y) {}
 
 void Camera::follow(int targetX, int targetY) {
-    x = targetX;
-    y = targetY;
+    this->x = targetX;
+    this->y = targetY;
 }
 
 void Camera::setPosition(int x, int y) {
